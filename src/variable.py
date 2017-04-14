3422f2a8edd78c4565cc72499b272881c8432d59
Farm <lucas.carotta@outlook.com>
2017-04-14 15:15:09 -0300

    First commit

---
+++ src/variable.py
@@ -0,0 +1,11 @@
+greeting = "Hello"
+print( greeting )
+print( input( "Write anything: " ) )
+number = 3
+print( number )
+age = input( "Enter your age: " )
+new_age = int( age ) + 20.0
+print( new_age )
+
+#   ----    Math    ----    #
+print( 3**2 )
