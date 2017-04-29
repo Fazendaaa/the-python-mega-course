2b4cf274fe8bb21a2b786fc5c4c9e24d1629bb4f
Farm <lucas.carotta@outlook.com>
2017-04-28 21:26:42 -0300

    Deploying web app.

---
+++ src/webApp/demo/webApp.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask( __name__ )
+
+@app.route( '/' )
+def home():
+    return render_template( 'home.html' )
+
+@app.route( '/about/' )
+def about():
+    return render_template( 'about.html' )
+
+if '__main__' == __name__:
+    app.run( debug = True )
+
+#   ------------------------------   EOF   ---------------------------------   #
